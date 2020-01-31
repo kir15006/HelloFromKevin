573e3c1d1c6d820566859cfda8ab22a3b27d6312
Elijah Elliott <elijahelliott@Elijahs-MacBook-Pro.local>
2020-01-31 16:00:38 -0700

    added my name

--- src/kirby/HelloFromKevin.java
+++ src/kirby/HelloFromKevin.java
@@ -3,6 +3,7 @@ package kirby;
 public class HelloFromKevin {
     public static void main(String[] args) {
         System.out.println("Hello Jared, Elijah, Jarek, and Jonathan, my name is Kevin");
+        System.out.println("Hello Kevin, y name is Elijah");
     }
 }
 
