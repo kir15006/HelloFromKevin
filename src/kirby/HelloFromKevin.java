d3e51cabde43a476a60a1e8d64d84699a9889899
Kevin <kir15006@byui.edu>
2020-01-31 16:21:01 -0700

    just added a random line to remove

--- src/kirby/HelloFromKevin.java
+++ src/kirby/HelloFromKevin.java
@@ -5,6 +5,7 @@ public class HelloFromKevin {
         System.out.println("Hello Jared, Elijah, Jarek, and Jonathan, my name is Kevin");
         System.out.println("Hello Kevin, y name is Elijah");
         System.out.println("Hello Kevin, my name is Jared");
+        System.out.println("just a random change to remove");
     }
 }
 
